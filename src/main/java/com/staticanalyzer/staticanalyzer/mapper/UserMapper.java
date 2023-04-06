d2cf699313287a9022ab5dada4fa592c728b2e28
LambdaLe <iswangli@foxmail.com>
2023-04-06 12:41:06 +0800

    fix: 移除UserMapper中的selectWithProjectById

--- src/main/java/com/staticanalyzer/staticanalyzer/mapper/UserMapper.java
+++ src/main/java/com/staticanalyzer/staticanalyzer/mapper/UserMapper.java
@@ -19,15 +19,6 @@ public interface UserMapper extends BaseMapper<User> {
             @Result(column = "id", property = "id"),
             @Result(column = "username", property = "username"),
             @Result(column = "password", property = "password"),
-            @Result(column = "id", property = "projectList", javaType = List.class, many = @Many(select = "com.staticanalyzer.staticanalyzer.mapper.ProjectMapper.selectByUserId"))
-    })
-    User selectWithProjectById(@Param("id") int id);
-
-    @Select("SELECT * FROM user WHERE id=#{id}")
-    @Results({
-            @Result(column = "id", property = "id"),
-            @Result(column = "username", property = "username"),
-            @Result(column = "password", property = "password"),
             @Result(column = "id", property = "projectIdList", javaType = List.class, many = @Many(select = "com.staticanalyzer.staticanalyzer.mapper.ProjectMapper.selectIdByUserId"))
     })
     User selectWithProjectIdById(@Param("id") int id);
