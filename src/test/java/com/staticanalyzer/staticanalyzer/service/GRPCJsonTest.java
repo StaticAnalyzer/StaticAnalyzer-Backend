20ca5c018527378c255ecf998fd101be7460231d
LambdaLe <iswangli@foxmail.com>
2023-04-26 16:18:11 +0800

    test: 添加gRPC序列化相关测试

---
+++ src/test/java/com/staticanalyzer/staticanalyzer/service/GRPCJsonTest.java
@@ -0,0 +1,108 @@
+package com.staticanalyzer.staticanalyzer.service;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import com.staticanalyzer.algservice.AlgAnalyseResult;
+import com.staticanalyzer.algservice.AnalyseResponse;
+import com.staticanalyzer.algservice.AnalyseResultEntry;
+import com.staticanalyzer.algservice.FileAnalyseResults;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GRPCJsonTest {
+
+
+    @Test
+    void TestGrpcJsonSerialization() throws InvalidProtocolBufferException {
+        AnalyseResponse.Builder builder = AnalyseResponse.newBuilder();
+        builder.setCode(0);
+        builder.setMsg("success");
+
+        AlgAnalyseResult.Builder algResultBuilder = AlgAnalyseResult.newBuilder();
+        algResultBuilder.setAnalyseType("UninitializedVariable");
+        algResultBuilder.setCode(0);
+        algResultBuilder.setMsg("success");
+
+        FileAnalyseResults.Builder fileResultBuilder = FileAnalyseResults.newBuilder();
+        AnalyseResultEntry.Builder entryBuilder = AnalyseResultEntry.newBuilder();
+        entryBuilder.setStartLine(1);
+        entryBuilder.setEndLine(1);
+        entryBuilder.setStartColumn(1);
+        entryBuilder.setEndColumn(3);
+        entryBuilder.setMessage("Uninitialized variable: a");
+        entryBuilder.setSeverity("error");
+        fileResultBuilder.addAnalyseResults(entryBuilder.build());
+
+        algResultBuilder.putFileAnalyseResults("main.cpp", fileResultBuilder.build());
+        builder.addAlgAnalyseResults(algResultBuilder.build());
+
+        String json = JsonFormat.printer().includingDefaultValueFields().print(builder.build());
+        System.out.println(json);
+    }
+
+    @Test
+    void TestGrpcJsonDeserialize() throws InvalidProtocolBufferException {
+        String json = """
+{
+    "code": 0,
+    "msg": "分析完成",
+    "algAnalyseResults": [
+        {
+            "analyseType": "UninitializedVariable",
+            "code": 0,
+            "msg": "",
+            "fileAnalyseResults": {
+                "main.cpp": {
+                    "analyseResults": [
+                        {
+                            "startLine": 1,
+                            "startColumn": 1,
+                            "endLine": 1,
+                            "endColumn": 2,
+                            "severity": "Error",
+                            "message": "error message"
+                        },
+                        {
+                            "startLine": 4,
+                            "startColumn": 2,
+                            "endLine": 4,
+                            "endColumn": 8,
+                            "severity": "Warning",
+                            "message": "error message"
+                        }
+                    ]
+                },
+                "src/a.c": {
+                    "analyseResults": [
+                        {
+                            "startLine": 1,
+                            "startColumn": 3,
+                            "endLine": 1,
+                            "endColumn": 5,
+                            "severity": "Error",
+                            "message": "error message"
+                        },
+                        {
+                            "startLine": 4,
+                            "startColumn": 2,
+                            "endLine": 4,
+                            "endColumn": 8,
+                            "severity": "Info",
+                            "message": "error message"
+                        }
+                    ]
+                }
+            }
+        }
+    ]
+}
+                """;
+
+        AnalyseResponse.Builder builder = AnalyseResponse.newBuilder();
+        JsonFormat.parser().merge(json, builder);
+        AnalyseResponse response_regen = builder.build();
+
+        System.out.println(response_regen.toString());
+    }
+}
