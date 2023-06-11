9bfea17d3b66763fef91e476b6c68f7d4be01c20
Yang Yu <three1518@163.com>
2023-06-11 22:38:12 +0800

    chore: 增加随机性、鲁棒性更强的测试

--- src/test/java/com/staticanalyzer/staticanalyzer/utils/JwtTest.java
+++ src/test/java/com/staticanalyzer/staticanalyzer/utils/JwtTest.java
@@ -8,19 +8,20 @@ import io.jsonwebtoken.SignatureAlgorithm;
 
 // 本测试与Spring Boot无关
 public class JwtTest {
+    
+    private static java.util.Random JWT_RANDOM = new java.util.Random();
 
     @org.junit.Test
     public void testJwt() {
-        java.util.Random random = new java.util.Random();
         byte[] keyBytes = new byte[256];
 
         // 随机化测试
-        random.nextBytes(keyBytes);
+        JWT_RANDOM.nextBytes(keyBytes);
         java.security.Key key = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
-        java.time.Duration expiration = java.time.Duration.parse(String.format("PT%dS", random.nextInt(600)));
+        java.time.Duration expiration = java.time.Duration.parse(String.format("PT%dS", JWT_RANDOM.nextInt(600)));
 
         for (int i = 0; i < 100; i++) {
-            int id = random.nextInt();
+            int id = JWT_RANDOM.nextInt(32768);
             String jws = JwtUtils.generateJws(key, expiration, id);
             assertEquals(JwtUtils.parseJws(key, jws), id);
         }
